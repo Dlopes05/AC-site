693d1fa181bf7ff75f550c37395c00335c444c10
Dlopes05 <danniellopescosta@gmail.com>
2025-07-31 21:17:22 -0300

    a

--- AC_site/settings.py
+++ AC_site/settings.py
@@ -130,26 +130,30 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 # No final do arquivo, adicione ou modifique a seção de Mídia e Estáticos
+STATIC_URL = 'static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+
 if IS_PRODUCTION:
-    # Configuração para S3
+    # Configuração para S3 (ou Discos Persistentes)
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    
     # Configuração para os arquivos de mídia (fotos, musicas)
     DEFAULT_FILE_STORAGE = 'AC_site.storages.MediaStore'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 else:
     # Configuração local que você já tinha
-    STATIC_URL = 'static/'
-    STATIC_ROOT = BASE_DIR / 'staticfiles'
     MEDIA_URL = '/media/'
     MEDIA_ROOT = BASE_DIR / 'media'
 
 # O WhiteNoise continua sendo usado para os arquivos estáticos (CSS/JS)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+
 
